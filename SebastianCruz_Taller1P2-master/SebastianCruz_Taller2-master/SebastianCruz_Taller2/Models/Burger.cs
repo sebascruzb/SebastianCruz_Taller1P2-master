e12b7c193c890b52ab6de1ea5931521570236490
Sebastian Cruz <raul.cruz@udla.edu.ec>
2024-05-22 10:29:04 -0500

    Agregar archivos de proyecto.

---
+++ SebastianCruz_Taller1P2-master/SebastianCruz_Taller2-master/SebastianCruz_Taller2/Models/Burger.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SebastianCruz_Taller2.Models
+{
+    public class Burger
+    {
+        public int BurgerID { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+
+        public List<Promo>? Promo { get; set; }
+    }
+}
